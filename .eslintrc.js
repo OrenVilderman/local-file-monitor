80a6cb59a707c41ea30d45663d86e7b49c32e206
Oren Vilderman <OrenVilderman@gmail.com>
2022-03-05 21:02:14 +0200

    Added linter

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+    'max-len': 'off',
+    'eqeqeq': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-shadow': 'off',
+    'no-unused-expressions': 'off',
+    'no-promise-executor-return': 'off',
+  },
+};
